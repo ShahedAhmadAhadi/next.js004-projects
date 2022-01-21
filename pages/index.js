eda514317a787dd8a9cebdf9eef178e773dce6b5
Shahed Ahmad <shaheda.a16@gmail.com>
2022-01-22 00:55:28 +0430

    data return from fetching banner updated, props from statics logged success, issue removed

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,24 @@ import Image from 'next/image'
 import { Flex, Box, Text, Button } from '@chakra-ui/react'
 import { baseURL, fetchApi } from '../utils/fetchAPI'
 
-const Banner = ({ purpose, title1, title2, desc1, desc2, buttongText, linkName, imageUrl }) => {
-    <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
-        <Image src={imageUrl} alt='banner' width={500} height={300} />
-        <Box p="5">
-            <Text color="gray.500" fontSize="sm" fontWeight="medium">{purpose}</Text>
-            <Text fontSize="3xl" fontWeight="bold">{title1}<br />{title2}</Text>
-            <Text fontSize="sm" paddingTop="3" paddingBottom="3" color="gray.700">{desc1}<br />{desc2}</Text>
-            <Button fontSize="xl">
-                <Link href={linkName}>{buttongText}</Link>
-            </Button>
-        </Box>
+export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
+    <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
+      <Image src={imageUrl} width={500} height={300} alt='Banner' />
+      <Box p='5'>
+        <Text color='gray.500' fontSize='sm' fontWeight='medium'>{purpose}</Text>
+        <Text fontSize='3xl' fontWeight='bold'>{title1}<br />{title2}</Text>
+        <Text fontSize='lg' paddingTop='3' paddingBottom='3' color='gray.700'>{desc1}<br />{desc2}</Text>
+        <Button fontSize='xl' bg="blue.300" color="white">
+          <Link href={linkName}><a>{buttonText}</a></Link>
+        </Button>
+      </Box>
     </Flex>
-}
+  );
 
-export default function Home() {
+export default function Home( {propertiesForSale, propertiesForRent}) {
+    console.log(propertiesForSale, propertiesForRent)
     return (
-        <Box className={styles.container}>
+        <Box className="">
             <Banner
                 purpose='RENT A HOME'
                 title1='Rental Homes for'
@@ -30,6 +31,9 @@ export default function Home() {
                 linkName='/search?purpose=for-rent'
                 imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
             />
+            <Flex flexWrap={"wrap"}>
+
+            </Flex>
             <Banner
                 purpose='BUY A HOME'
                 title1=' Find, Buy & Own Your'
@@ -40,13 +44,16 @@ export default function Home() {
                 linkName='/search?purpose=for-sale'
                 imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
             />
+            <Flex flexWrap={"wrap"}>
+
+            </Flex>
         </Box>
     )
 }
 
 export async function getStaticProps() {
-    const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`);
-    const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`);
+    const propertyForSale = await fetchApi(`${baseURL}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`);
+    const propertyForRent = await fetchApi(`${baseURL}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`);
   
     return {
       props: {
--- utils/fetchAPI.js
+++ utils/fetchAPI.js
@@ -2,11 +2,13 @@ import axios from "axios";
 
 export const baseURL = 'https://' + process.env.API_HOST
 
-export default fetchApi = async (url) => {
+export const fetchApi = async (url) => {
     const { data } = await axios.get((url), {
         headers: {
             'x-rapidapi-host': process.env.API_HOST,
             'x-rapidapi-key': process.env.API_PASSWORD
         }
     })
+
+    return data
 }
