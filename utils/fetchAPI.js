e471fe3bee3c2b5f4cd86b23c15d7882911765ab
Shahed Ahmad <shaheda.a16@gmail.com>
2022-01-21 22:41:43 +0430

    Boxes for core front added with requrest to api

---
+++ utils/fetchAPI.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const baseURL = 'https://' + process.env.API_HOST
+
+export default fetchApi = async (url) => {
+    const { data } = await axios.get((url), {
+        headers: {
+            'x-rapidapi-host': process.env.API_HOST,
+            'x-rapidapi-key': process.env.API_PASSWORD
+        }
+    })
+}
