7e693287f3c71cec6f145106572d9851198a8052
Shahed Ahmad <shaheda.a16@gmail.com>
2022-01-24 22:17:47 +0430

    refactor

--- utils/fetchAPI.js
+++ utils/fetchAPI.js
@@ -1,14 +1,14 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const baseURL = 'https://' + process.env.API_HOST
+export const baseURL = 'https://' + process.env.API_HOST;
 
 export const fetchApi = async (url) => {
-    const { data } = await axios.get((url), {
+    const { data } = await axios.get(url, {
         headers: {
             'x-rapidapi-host': process.env.API_HOST,
-            'x-rapidapi-key': process.env.API_PASSWORD
-        }
-    })
+            'x-rapidapi-key': process.env.API_PASSWORD,
+        },
+    });
 
-    return data
-}
+    return data;
+};
