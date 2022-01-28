ab2d85a5e782572230850a31681b104e0e4e9a54
Shahed Ahmad <shaheda.a16@gmail.com>
2022-01-28 20:46:15 +0430

    Image Scroller property if Image exists added

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -4,19 +4,19 @@ import { baseUrl, fetchApi } from '../../utils/fetchApi';
 import ImageScrollbar from '../../components/ImageScrollbar';
 
 const PropertyDetails = ({ propertyDetails }) => (
-  <Box maxWidth='1000px' margin='auto' p='4'>
-    
-  </Box>
+    <Box maxWidth='1000px' margin='auto' p='4'>
+        {photos && <ImageScrollbar data={photos} />}
+    </Box>
 );
 
 export default PropertyDetails;
 
 export async function getServerSideProps({ params: { id } }) {
-  const data = await fetchApi(`${baseUrl}/properties/detail?externalID=${id}`);
-  
-  return {
-    props: {
-      propertyDetails: data,
-    },
-  };
+    const data = await fetchApi(`${baseUrl}/properties/detail?externalID=${id}`);
+
+    return {
+        props: {
+            propertyDetails: data,
+        },
+    };
 }
