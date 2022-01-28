cb90e97bc4500f91228e7f2f9c34f43908523267
Shahed Ahmad <shaheda.a16@gmail.com>
2022-01-28 20:40:49 +0430

    new property for every item in api

---
+++ pages/property/[id].js
@@ -0,0 +1,22 @@
+import { Box, Flex, Spacer, Text } from '@chakra-ui/layout';
+
+import { baseUrl, fetchApi } from '../../utils/fetchApi';
+import ImageScrollbar from '../../components/ImageScrollbar';
+
+const PropertyDetails = ({ propertyDetails }) => (
+  <Box maxWidth='1000px' margin='auto' p='4'>
+    
+  </Box>
+);
+
+export default PropertyDetails;
+
+export async function getServerSideProps({ params: { id } }) {
+  const data = await fetchApi(`${baseUrl}/properties/detail?externalID=${id}`);
+  
+  return {
+    props: {
+      propertyDetails: data,
+    },
+  };
+}
--- pages/search.js
+++ pages/search.js
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
-import Image from 'next/image'
+import Image from 'next/image';
 import { Flex, Box, Text, Icon } from '@chakra-ui/react';
 import { BsFilter } from 'react-icons/bs';
 
 import Property from '../components/Property';
 import SearchFilters from '../components/SearchFilters';
 import { baseURL, fetchApi } from '../utils/fetchApi';
-import noresult from '../assets/images/noresult.svg'
+import noresult from '../assets/images/noresult.svg';
 
 const Search = ({ properties }) => {
     const [searchFilters, setSearchFilters] = useState(false);
@@ -17,30 +17,40 @@ const Search = ({ properties }) => {
         <Box>
             <Flex
                 onClick={() => setSearchFilters(!searchFilters)}
-                cursor='pointer'
-                bg='gray.100'
-                borderBottom='1px'
-                borderColor='gray.200'
-                p='2'
-                fontWeight='black'
-                fontSize='lg'
-                justifyContent='center'
-                alignItems='center'
+                cursor="pointer"
+                bg="gray.100"
+                borderBottom="1px"
+                borderColor="gray.200"
+                p="2"
+                fontWeight="black"
+                fontSize="lg"
+                justifyContent="center"
+                alignItems="center"
             >
                 <Text>Search Property By Filters</Text>
-                <Icon paddingLeft='2' w='7' as={BsFilter} />
+                <Icon paddingLeft="2" w="7" as={BsFilter} />
             </Flex>
             {searchFilters && <SearchFilters />}
-            <Text fontSize='2xl' p='4' fontWeight='bold'>
+            <Text fontSize="2xl" p="4" fontWeight="bold">
                 Properties {router.query.purpose}
             </Text>
-            <Flex flexWrap='wrap'>
-                {properties.map((property) => <Property property={property} key={property.id} />)}
+            <Flex flexWrap="wrap">
+                {properties.map((property) => (
+                    <Property property={property} key={property.id} />
+                ))}
             </Flex>
             {properties.length === 0 && (
-                <Flex justifyContent='center' alignItems='center' flexDir='column' marginTop='5' marginBottom='5'>
-                    <Image src={noresult} alt='no_result' />
-                    <Text fontSize='xl' marginTop='3'>No Result Found.</Text>
+                <Flex
+                    justifyContent="center"
+                    alignItems="center"
+                    flexDir="column"
+                    marginTop="5"
+                    marginBottom="5"
+                >
+                    <Image src={noresult} alt="no_result" />
+                    <Text fontSize="xl" marginTop="3">
+                        No Result Found.
+                    </Text>
                 </Flex>
             )}
         </Box>
@@ -59,7 +69,9 @@ export async function getServerSideProps({ query }) {
     const locationExternalIDs = query.locationExternalIDs || '5002';
     const categoryExternalID = query.categoryExternalID || '4';
 
-    const data = await fetchApi(`${baseURL}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`);
+    const data = await fetchApi(
+        `${baseURL}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`
+    );
 
     return {
         props: {
