177b942f467a0e4c59dd21a6e20220bdc91e41f4
Shahed Ahmad <shaheda.a16@gmail.com>
2022-01-29 21:23:07 +0430

    app ready for Deploy

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,25 +1,23 @@
-import Head from "next/head";
-import { Box } from "@chakra-ui/react";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
+import Head from 'next/head';
+import { Box } from '@chakra-ui/react';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 const Layout = ({ children }) => (
     <>
         <Head>
             <title>Real State</title>
         </Head>
-        <Box maxWidth={"1280px"} m={'auto'}>
+        <Box maxWidth={'1280px'} m={'auto'}>
             <header>
                 <Navbar />
             </header>
-            <main>
-                {children}
-            </main>
+            <main>{children}</main>
             <footer>
                 <Footer />
             </footer>
         </Box>
     </>
-)
+);
 
-export default Layout
+export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -15,7 +15,7 @@ function MyApp({ Component, pageProps }) {
   Router.events.on('routeChangeComplete', () => {
     NProgress.done();
   });
-  
+
   return <>
     <Head>
       <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css' integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==' crossOrigin='anonymous' referrerPolicy='no-referrer' />
